8c2108fcae56922cf893ec48e51acfee03e00e01
shouhei.katsuyama <k.shouhei0711@gmail.com>
2025-01-14 23:28:26 +0900

    helloworld画面表示

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JankenJudgeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// hello world画面の表示
+Route::controller(JankenJudgeController::class)->group(function () {
+    Route::get('/index', 'index');
+});
+
+// Route::get('/hello', function () {
+//     return 'HELLO WORLD!!';
+// });
