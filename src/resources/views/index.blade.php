b96ee3abde697fd517020ff30e37420ffab3f84f
shouhei.katsuyama <k.shouhei0711@gmail.com>
2025-01-19 00:23:23 +0900

    ホーム画面作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,11 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>ホーム</title>
 </head>
 
 <body>
-    <h1>HELLOWORLD!!</h1>
+    <div style="text-align: center;">
+        <h1 style="margin: 5%;">ジャンケンを始めましょう！！</h1>
+        <div style="display: flex; justify-content: space-around;">
+            <div>
+                <img style="width: 60%; cursor: pointer;" src="{{ asset('img/1501948.jpg') }}" alt="">
+            </div>
+            <div>
+                <img style="width: 60%; cursor: pointer;" src="{{ asset('img/1501947.jpg') }}" alt="">
+            </div>
+            <div>
+                <img style="width: 60%; cursor: pointer;" src="{{ asset('img/1501953.jpg') }}" alt="">
+            </div>
+        </div>
+        <div>
+            <button
+                style="font-size: 200%; margin: 5%; color: white; background-color: black; border-radius: 5px;">対戦履歴</button>
+        </div>
+    </div>
+
 </body>
 
 </html>
