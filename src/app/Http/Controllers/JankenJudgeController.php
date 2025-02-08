9e0a45d280e04602f847b51e8086fd4127a23c52
shouhei.katsuyama <k.shouhei0711@gmail.com>
2025-02-08 12:17:46 +0900

    対戦履歴保存処理作成

--- src/app/Http/Controllers/JankenJudgeController.php
+++ src/app/Http/Controllers/JankenJudgeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\history;
 
 class JankenJudgeController extends Controller
 {
@@ -29,6 +30,14 @@ class JankenJudgeController extends Controller
         } else {
             $result = '負け！！';
         }
+
+        // 勝敗のデータをデータベースに登録する
+        $history = new history();
+        $history->user_hand = $userHand;
+        $history->cpu_hand = $cpuHand;
+        $history->result = $result;
+        $history->save();
+ 
     
         return view('result', compact('result', 'userHand', 'cpuHand'));
     }
